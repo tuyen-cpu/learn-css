0d576e3faa41fc0689fc7c50cb5ae798dda412d3
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-01-25 01:07:59 +0700

    init proj, lession 1-5: darkmode when toggle

---
+++ tailwind-css-proj/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        35: "35px",
+      },
+      colors: {
+        gray33: "#333",
+      },
+    },
+  },
+  plugins: [],
+};
